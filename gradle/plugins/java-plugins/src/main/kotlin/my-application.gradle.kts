a58d1826c3ac453879937414af5d49046b1ad31c
OldRabbit736 <sylvan0212@gmail.com>
2022-08-13 20:32:27 +0900

    completed: 10. Implement your own Build Logic in a Task

--- gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
@@ -1,6 +1,18 @@
+import com.example.gradle.JarCount
+
 plugins {
     id("application")
     id("my-java-base")
+}
+
+tasks.register<JarCount>("countJars") {
+    group = "My Group"
+    description = "Counts!"
+
+    allJars.from(tasks.jar)
+    allJars.from(configurations.runtimeClasspath)
+
+    countFile.set(layout.buildDirectory.file("gen/count.txt"))
 }
 
 // 이미 있는 task type ZIP 을 사용하여 새로운 "bundle" 이라는 task 를 생성 (타입을 지정하지 않고 생성할 수도 있다.)
@@ -14,6 +26,7 @@ tasks.register<Zip>("bundle") {
 
     // 아래와 같이 jar task 의 아웃풋을 인수로 설정한다.
     from(tasks.jar)
+    // runtimeClasspath 의 모듈의 jar 를 인수로 설정한다.
     from(configurations.runtimeClasspath)
 
     destinationDirectory.set(layout.buildDirectory.dir("distribution"))
